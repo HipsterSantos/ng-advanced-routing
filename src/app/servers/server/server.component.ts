c77a78909d833b90402d39248a65b02c342878d7
HipsterSantos <santoscampos269@gmail.com>
2024-04-16 19:50:56 -0400

    feat: update edit-service

--- src/app/servers/server/server.component.ts
+++ src/app/servers/server/server.component.ts
@@ -1,12 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { ServersService } from '../servers.service';
 
 @Component({
   selector: 'app-server',
-  standalone: true,
-  imports: [],
   templateUrl: './server.component.html',
-  styleUrl: './server.component.scss'
+  styleUrls: ['./server.component.css']
 })
-export class ServerComponent {
+export class ServerComponent implements OnInit {
+  server!: {id: number, name: string, status: string};
+
+  constructor(private serversService: ServersService) { }
+
+  ngOnInit() {
+    this.server = this.serversService.getServer(1)!;
+  }
 
 }
