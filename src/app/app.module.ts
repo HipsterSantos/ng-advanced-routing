c1e35cb9647e5e8fc5c10b998d34cd6b0e21078f
HipsterSantos <santoscampos269@gmail.com>
2024-04-16 20:29:27 -0400

    update: router-outlet

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,7 +11,22 @@ import { UserComponent } from './users/user/user.component';
 import { EditServerComponent } from './servers/edit-server/edit-server.component';
 import { ServerComponent } from './servers/server/server.component';
 import { ServersService } from './servers/servers.service';
+import { RouterModule, Routes } from '@angular/router';
 
+const routes: Routes = [
+  {
+    component: UsersComponent,
+    path:'user'
+  },
+  {
+    component: ServersComponent,
+    path:'server'
+  },
+  {
+    component: HomeComponent,
+    path:''
+  }
+]
 @NgModule({
   declarations: [
     AppComponent,
@@ -25,6 +40,7 @@ import { ServersService } from './servers/servers.service';
   imports: [
     BrowserModule,
     FormsModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [ServersService],
   bootstrap: [AppComponent]
