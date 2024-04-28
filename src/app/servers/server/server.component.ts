0ac215c56f130ebfdd7fd3a49e7cdc808963153b
HipsterSantos <santoscampos269@gmail.com>
2024-04-28 02:20:53 +0100

    chores: update getting users dynamically from router

--- src/app/servers/server/server.component.ts
+++ src/app/servers/server/server.component.ts
@@ -11,23 +11,28 @@ import { ActivatedRoute, Params, Router } from '@angular/router';
 export class ServerComponent implements OnInit {
   server!: {id: number, name: string, status: string};
   serverId!: number;
+  allowEdit: boolean = false;
   constructor(private route: Router, private serversService: ServersService,private activeRoute: ActivatedRoute) { }
 
   ngOnInit() {
     this.serverId  = +this.activeRoute.snapshot.params['id']
     this.server = this.serversService.getServer(this.serverId)!;
+    this.allowEdit = this.activeRoute.snapshot.queryParams['allowEdit']
     
     this.activeRoute.params.subscribe( (params: Params)=>{
-      this.server = this.serversService.getServer(params['id'])!;
+      this.server = this.serversService.getServer(+params['id'])!;
+    })
+    this.activeRoute.queryParams.subscribe( (params: Params)=>{
+      this.allowEdit = params['allowEdit']
     })
     console.log('server - queryparams', this.activeRoute.snapshot.queryParamMap)
     console.log('server - fragment', this.activeRoute.snapshot.fragment)
   }
   onEdit(){
-    this.route.navigate(['/server',''],{
+    this.route.navigate(['/server',this.serverId,'edit'],{
       relativeTo: this.activeRoute ,
       preserveFragment: true,
-      queryParamsHandling:'preserve',
+      queryParamsHandling:'merge',
       queryParams:{allowEdit: 1},
       fragment:'top'
     })
